dd84d1a9be7748c2d562f33b6e062f9237459578
simonarnaud <simonarnaud@192.168.1.17>
2025-10-19 15:17:57 +0200

    Ajout de tout les .java nécessaire

--- ICarte.java
+++ ICarte.java
@@ -2,11 +2,11 @@ package wargame;
 import java.awt.Graphics;
 public interface ICarte {
 	Element getElement(Position pos);
-	Position trouvePositionVide(); // Trouve aléatoirement une position vide sur la carte
+	Position trouvePositionVide(); // Trouve alÃ©atoirement une position vide sur la carte
 	Position trouvePositionVide(Position pos); // Trouve une position vide choisie
-								// aléatoirement parmi les 8 positions adjacentes de pos
-	Heros trouveHeros(); // Trouve aléatoirement un héros sur la carte
-	Heros trouveHeros(Position pos); // Trouve un héros choisi aléatoirement
+								// alï¿½atoirement parmi les 8 positions adjacentes de pos
+	Heros trouveHeros(); // Trouve alï¿½atoirement un hï¿½ros sur la carte
+	Heros trouveHeros(Position pos); // Trouve un hï¿½ros choisi alï¿½atoirement
 									 // parmi les 8 positions adjacentes de pos
 	boolean deplaceSoldat(Position pos, Soldat soldat);
 	void mort(Soldat perso);
